827da0014eee9fc84c68d55fe589fff8245ccced
Sidhdi7 <71689692+Sidhdi7@users.noreply.github.com>
2025-04-09 18:28:08 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub Actions without Gradle!");
+       System.out.println("Hello, Sidhdi!");
+
     }
 }
